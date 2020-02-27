b83f0c61f857bae5b5e9af347bf7e498e4f5425a
Kai <lyons.safyre@gmail.com>
2020-02-27 06:35:17 -0700

    Able to print strings, isn't perfect

--- kailang.cpp
+++ kailang.cpp
@@ -10,6 +10,26 @@
 #include <algorithm>
 using namespace std;
 
+// This section deals with string whitespace
+// ---------------------------------------------------------
+const string whiteSpace =" \n\r\t\f\v";
+
+string itrim(string const &str){
+    size_t start = str.find_first_not_of(whiteSpace);
+    return (start == string::npos) ? "" : str.substr(start);
+}
+
+string ttrim(string const &str){
+    size_t finish = str.find_last_not_of(whiteSpace);
+    return (finish == string::npos) ? "" : str.substr(finish);
+}
+
+string trim(const string &str){
+    return ttrim(itrim(str));
+}
+// ---------------------------------------------------------
+
+// Find the extention of the file
 bool fileExt(string const &filename){
     string ext = ".kai";
     if(filename.length() > ext.length()){
@@ -39,12 +59,15 @@ int main(){
     ifstream kaicode (filename);
     if (kaicode.is_open()){
         while (getline (kaicode, line)){
-            line.erase(remove(line.begin(), line.end(), ' '), line.end());
+            trim(line);
+            // line.erase(remove(line.begin(), line.begin(), ' '), line.begin());
             // Check to find a "p" keyword (the keyword for print)
             if(line.rfind("p", 0) == 0){
                 if(line.find("\"")){
                     line.erase(remove(line.begin(), line.end(), 'p'), line.end());
+                    trim(line);
                     line.erase(remove(line.begin(), line.end(), '\"'), line.end());
+                    trim(line);
                     cout << line << endl;
                 }
             } else {
