ea9152a136d3935ca24e11da3ef72d3c7afc8731
Kai <lyons.safyre@gmail.com>
2020-02-28 09:41:14 -0700

    print line ability (non-whitespace)

--- kailang.cr
+++ kailang.cr
@@ -7,11 +7,18 @@ ARGV.each do |key|
     end
 end
 
+def print(line)
+    line = line.split("p", remove_empty: true)
+    line = line.join(",")
+    line = line.lstrip()
+    line = line.strip("\"")
+    puts line
+end
+
 def main (filename)
     File.each_line filename do |line|
-        puts line
         if line.starts_with?("p")
-            puts "Found print function"
+            print line
         end
         if line.starts_with?("f")
             puts "Found for/while loop"
