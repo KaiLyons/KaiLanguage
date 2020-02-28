8310871100e8af66ce85815ae52395cfa3056910
Kai <lyons.safyre@gmail.com>
2020-02-28 09:28:18 -0700

    get main keywords [that are in testing]

--- kailang.cr
+++ kailang.cr
@@ -10,5 +10,17 @@ end
 def main (filename)
     File.each_line filename do |line|
         puts line
+        if line.starts_with?("p")
+            puts "Found print function"
+        end
+        if line.starts_with?("f")
+            puts "Found for/while loop"
+        end
+        if line.starts_with?("v")
+            puts "found variable"
+        end
+        if line.starts_with?("e")
+            puts "found end"
+        end
     end
 end
