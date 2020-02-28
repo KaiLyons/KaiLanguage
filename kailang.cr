e621ad3fabdf8e99bc96cf3d1a9cdfdbc5ec4ea5
Kai <lyons.safyre@gmail.com>
2020-02-28 09:19:38 -0700

    It is now able to print the file contents

--- kailang.cr
+++ kailang.cr
@@ -1,7 +1,13 @@
 ARGV.each do |key|
     if key.ends_with?(".kai")
-        puts "#{key}"
+        filename = "#{key}"
+        main filename
     else 
         puts "wrong file type"
     end
+end
+
+def main (filename)
+    file = File.read filename
+    puts file
 end
