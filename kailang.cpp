f6e9cd41633fc7d112b1cb7ef12edc1f64213733
Kai <lyons.safyre@gmail.com>
2020-02-28 06:54:46 -0700

    Dealt with indentation issues

--- kailang.cpp
+++ kailang.cpp
@@ -10,25 +10,6 @@
 #include <algorithm>
 using namespace std;
 
-// This section deals with string whitespace
-// ---------------------------------------------------------
-const string whiteSpace =" \n\r\t\f\v";
-
-string itrim(string const &str){
-    size_t start = str.find_first_not_of(whiteSpace);
-    return (start == string::npos) ? "" : str.substr(start);
-}
-
-string ttrim(string const &str){
-    size_t finish = str.find_last_not_of(whiteSpace);
-    return (finish == string::npos) ? "" : str.substr(finish);
-}
-
-string trim(const string &str){
-    return ttrim(itrim(str));
-}
-// ---------------------------------------------------------
-
 // Find the extention of the file
 bool fileExt(string const &filename){
     string ext = ".kai";
@@ -61,13 +42,17 @@ int main(){
         while (getline (kaicode, line)){
             trim(line);
             // line.erase(remove(line.begin(), line.begin(), ' '), line.begin());
+            while (line.rfind(" ", 0) == 0){
+                line.erase(0,1);
+            }
             // Check to find a "p" keyword (the keyword for print)
             if(line.rfind("p", 0) == 0){
                 if(line.find("\"")){
                     line.erase(remove(line.begin(), line.end(), 'p'), line.end());
-                    trim(line);
+                    if (line.rfind(" ", 0) == 0){
+                        line.erase(0,1);
+                    }
                     line.erase(remove(line.begin(), line.end(), '\"'), line.end());
-                    trim(line);
                     cout << line << endl;
                 }
             } else {
